dada1bcb2e691164bb58674bc7d0b9c4a48825d2
Errant <erigidissimus@gmail.com>
2025-08-27 15:27:04 +0800

    Initial commit

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LC_LOCAL
+#include "parser.hpp"
+#else
+#define dbg(...)
+#endif
+
+class Solution {
+public:
+  void test() {}
+};
+
+#ifdef LC_LOCAL
+int main() {
+  run(&Solution::test); // adjust method name per problem
+}
+#endif
